75a7837ba939fa12a3f66f8eb4eda3176a8deaf7
Tommaso Diotalevi <tommaso.diotalevi@gmail.com>
2026-08-13 23:24:09 +0200

    Change spawn with Rucio aud

--- jupyterlab/spawn.sh
+++ jupyterlab/spawn.sh
@@ -2,18 +2,18 @@
 
 curl -d grant_type=refresh_token \
    -u $IAM_CLIENT_ID:$IAM_CLIENT_SECRET \
-   -d audience="https://wlcg.cern.ch/jwt/v1/any" \
+   -d audience="rucio" \
    -d refresh_token=$REFRESH_TOKEN \
-   -d scope="openid profile wlcg wlcg.groups" \
+   -d scope="openid profile" \
    https://iam-et.cloud.cnaf.infn.it/token \
    | tee /tmp/response | jq .access_token |  tr -d '"' |  tr -d '\n'> /tmp/token
 
 while true; do
     curl -d grant_type=refresh_token \
         -u $IAM_CLIENT_ID:$IAM_CLIENT_SECRET \
-        -d audience="https://wlcg.cern.ch/jwt/v1/any" \
+        -d audience="rucio" \
         -d refresh_token=$REFRESH_TOKEN \
-        -d scope="openid profile wlcg wlcg.groups" \
+        -d scope="openid profile" \
         https://iam-et.cloud.cnaf.infn.it/token \
         | tee /tmp/response | jq .access_token |  tr -d '"' |  tr -d '\n'> /tmp/token_tmp \
     && cp /tmp/token_tmp /tmp/token
